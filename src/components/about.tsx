ecbc06cc33e6347eb89054505720fa44030e558f
flaviobernachia <flaviobernachia@gmail.com>
2024-03-19 01:29:57 -0300

    fix text

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -53,30 +53,29 @@ export const About: React.FC = () => {
 
       <div className='aboutContent'>
         <div className='aboutMe'>
-        <p>
-            Welcome to my portfolio!
-        </p>
-        <p>
-            My name is Flavio Nicolás Bernachia, and Im thrilled to welcome you to my digital space. Originally from the vibrant city of San Nicolás de los Arroyos, in the Buenos Aires Province, I now call the beautiful city of Rosario, Santa Fe, my home.
-        </p>
-        <p>
-            Im a passionate full-stack developer with a special focus on the world of web development. Recently, I had the honor of successfully completing the Certified Tech Developer program, a renowned collaboration between Digital House, Mercado Libre, and Globant. Through this program, Ive earned diplomas that attest to my commitment to excellence, such as "Professional Developer" and "Frontend Specialist".
-        </p>
-        <p>
-            In addition to my technical experience, Im also advancing in my studies of Law, reflecting my passion for lifelong learning and my desire to tackle diverse challenges.
-        </p>
-        <p>
-            I consider myself a creative and solution-oriented individual, always on the lookout for opportunities to grow and learn. Im excited about the possibility of working on innovative projects and contributing to the success of your team.
-            I invite you to explore my portfolio and discover more about my work and skills. I look forward to the opportunity to chat with you and discuss how I can be a valuable asset to your company!
-        </p>
-        <p>
-            Thank you for visiting my site and considering my profile!
-        </p>
-        <p>
-            Best regards,
-            Flavio Nicolás Bernachia
-          </p>
-        </div>
+    <p>
+      Welcome to my portfolio!
+    </p>
+    <p>
+      My name is Flavio Nicolás Bernachia, and Im thrilled to welcome you to my digital space. Originally from the vibrant city of San Nicolás de los Arroyos, in the Buenos Aires Province, I now call the beautiful city of Rosario, Santa Fe, my home.
+    </p>
+    <p>
+      Im a passionate full-stack developer with a special focus on the world of web development. Recently, I had the honor of successfully completing the Certified Tech Developer program, a renowned collaboration between Digital House, Mercado Libre, and Globant. Through this program, Ive earned diplomas that attest to my commitment to excellence, such as &quot;Professional Developer&quot; and &quot;Frontend Specialist&quot;.
+    </p>
+    <p>
+      In addition to my technical experience, Im also advancing in my studies of Law, reflecting my passion for lifelong learning and my desire to tackle diverse challenges.
+    </p>
+    <p>
+      I consider myself a creative and solution-oriented individual, always on the lookout for opportunities to grow and learn. Im excited about the possibility of working on innovative projects and contributing to the success of your team. I invite you to explore my portfolio and discover more about my work and skills. I look forward to the opportunity to chat with you and discuss how I can be a valuable asset to your company!
+    </p>
+    <p>
+      Thank you for visiting my site and considering my profile!
+    </p>
+    <p>
+      Best regards,
+      Flavio Nicolás Bernachia
+    </p>
+  </div>
         <div className='gallery'>
           <div className='smallImage'><Image className='gallery-image' width={230} height={230} src={foto3} alt="" ></Image></div>
         <div className='largeImage'><Image className='gallery-image'  width={230} height={320}src={foto4} alt="" ></Image></div>
